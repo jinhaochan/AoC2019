f273b1aea2e81e01540598bba2e7dad530d69ae8
Jin Hao <jinhao_@hotmail.com>
2019-12-10 02:43:07 -0800

    day 3 part 1

--- 3/solution2.py
+++ 3/solution2.py
@@ -108,14 +108,14 @@ if __name__=='__main__':
 
     # Inputs
     if test:
-        w1 = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
-        w2 = ['U62','R66','U55','R34','D71','R55','D58','R83']
+        #w1 = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
+        #w2 = ['U62','R66','U55','R34','D71','R55','D58','R83']
 
         #w1 = ['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51']
         #w2 = ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7']
 
-        #w1 = ['R8','U5','L5','D3']
-        #w2 = ['U7','R6','D4','L4']
+        w1 = ['R8','U5','L5','D3']
+        w2 = ['U7','R6','D4','L4']
     else:
         with open('input.txt', 'r') as f:
             w1 = f.readline().strip().split(',')
@@ -129,18 +129,19 @@ if __name__=='__main__':
 
     intersections = []
 
+    prior_lines = []
+
     for hori in w1h:
         for vert in w2v:
             intersect = line_compare(hori, vert)
             if intersect > 0:
-                intersections.append(intersect)
+                intersections.append({'hori': hori, 'vert': vert})
             
     for hori in w2h:
         for vert in w1v:
             intersect = line_compare(hori, vert)
             if intersect > 0:
-                intersections.append(intersect)
+                intersections.append({'hori': hori, 'vert': vert})
 
-    print(min(intersections))
     print(intersections)
 
